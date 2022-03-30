509805cced09672cde1c44c4742ef75e9e5a7b93
Yukta Yadav <yuktajanuary@gmail.com>
2022-03-30 11:57:33 +0530

    Seccond Commit in Odd No

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Please input 5 integer values:");
+        Scanner sc=new Scanner(System.in);
+        int[] array;
+        array=new int[5];
+        for (int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+            sc.nextLine();
+
+        }
+
+        MethodsInJava p=new MethodsInJava();
+        p.getOddValue(array);
+        System.out.println("Odd values");
+        p.displayOddValue(p.getOddValue(array));
+
     }
 }
--- Test/src/MethodsInJava.java
+++ Test/src/MethodsInJava.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class MethodsInJava {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodsInJava {
+
+    public static Integer[] getOddValue(int[] arr1){
+        List<Integer> result=new ArrayList<>();
+        for (int k=0;k<arr1.length;k++){
+            if (arr1[k]%2!=0){
+                //displayOddValue(arr1[k]);
+                result.add(arr1[k]);
+
+
+            }
+        }
+
+        Integer[] arr = new Integer[result.size()];
+        arr = result.toArray(arr);
+        return arr;
+    }
+
+    public static void displayOddValue(Integer[] value){
+        for (Integer i:value){
+            System.out.println(i);
+        }
+
+    }
+
 }
